4af5948767d024d8d109aa6d25375e17beddfe6d
Lancelot de Ferrière <wraitii@gmail.com>
2022-04-25 23:16:46 +0200

    rename Do Something button

--- frontend/src/components/session_key/Help.vue
+++ frontend/src/components/session_key/Help.vue
@@ -2,7 +2,7 @@
     <div class="bg-white">
         <h1>Async helper</h1>
         <p><button @click.stop="setSessionKey" class="bg-gray-500">Create a temp key</button></p>
-        <p><button @click.stop="doSomething" class="bg-gray-500">Do Something</button></p>
+        <p><button @click.stop="doSomething" class="bg-gray-500 my-1">Claim Worker</button></p>
         <p><input type="text" size="70" v-model="session_contract"/><button class="bg-black text-white rounded-md" @click="setSessionContract">Set Contract</button><br/> Test - {{ getSessionKey() && false || '' }} {{ sessionKeyData }}</p>
     </div>
 </template>
